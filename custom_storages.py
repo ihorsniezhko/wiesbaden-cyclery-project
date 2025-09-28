4d883c7ccf29a7460a626074b78e8398c1944eb7
ihorsniezhko <ihor.sniezhko@gmail.com>
2025-09-28 21:54:21 +0200

    Remove ACL settings for S3 bucket compatibility

--- custom_storages.py
+++ custom_storages.py
@@ -8,13 +8,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class StaticStorage(S3Boto3Storage):
     """Custom storage for static files"""
     location = 'static'
-    default_acl = 'public-read'
+    default_acl = None
 
 
 class MediaStorage(S3Boto3Storage):
     """Custom storage for media files"""
     location = 'media'
-    default_acl = 'public-read'
+    default_acl = None
     file_overwrite = False
     
     def __init__(self, *args, **kwargs):
--- wiesbaden_cyclery/settings.py
+++ wiesbaden_cyclery/settings.py
@@ -188,7 +188,7 @@ if USE_AWS:
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
