d949508c210f614347c8f769f633e4dbecf292ef
ihorsniezhko <ihor.sniezhko@gmail.com>
2025-10-04 17:04:42 +0200

    Add debug view to diagnose cart 500 error on Heroku
    - Added debug_cart view to test cart functionality step by step
    - Will help identify the exact cause of the 500 error on production
    - Tests get_or_create_cart function, cart context, and template variables
    - Provides detailed error information and traceback

--- debug_views.py
+++ debug_views.py
@@ -1,82 +1,11 @@
-"""
-Main views for Wiesbaden Cyclery
-"""
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    """
-    Homepage view
-    """
-    return render(request, 'home/index.html')
-
-
-def categories(request):
-    """
-    Categories page view
-    """
-    from products.models import Category
-    
-    all_categories = Category.objects.all()
-    
-    context = {
-        'all_categories': all_categories,
-    }
-    
-    return render(request, 'categories/categories.html', context)
-
-
-def privacy_policy(request):
-    """A view to return the privacy policy page"""
-    context = {
-        'last_updated': 'January 1, 2025',
-    }
-    return render(request, 'legal/privacy_policy.html', context)
-
-
-def terms_of_service(request):
-    """A view to return the terms of service page"""
-    context = {
-        'last_updated': 'January 1, 2025',
-    }
-    return render(request, 'legal/terms_of_service.html', context)
-
-
-def cookie_settings(request):
-    """A view to return the cookie settings page"""
-    return render(request, 'legal/cookie_settings.html')
-
-
-def robots_txt(request):
-    """A view to return the robots.txt file"""
-    template = loader.get_template('robots.txt')
-    return HttpResponse(template.render({'request': request}, request), content_type='text/plain')
-
-
-def seo_test(request):
-    """A view to return the SEO test page"""
-    return render(request, 'seo_test.html')
-
-
-def custom_404(request, exception):
-    """Custom 404 error page"""
-    return render(request, '404.html', status=404)
-
-
-def custom_500(request):
-    """Custom 500 error page"""
-    return render(request, '500.html', status=500)
-
+from django.shortcuts import render
+from shopping_cart.utils import get_or_create_cart
+from shopping_cart.models import Cart, CartItem
+import traceback
 
 def debug_cart(request):
     """Debug view to test cart functionality on Heroku"""
-    from django.http import HttpResponse
-    from shopping_cart.utils import get_or_create_cart
-    from shopping_cart.models import Cart, CartItem
-    import traceback
-    
     debug_info = []
     
     try:
@@ -126,5 +55,4 @@ def debug_cart(request):
         debug_info.append(f"\n✗ ERROR: {str(e)}")
         debug_info.append(f"Traceback:\n{traceback.format_exc()}")
     
-    from django.http import HttpResponse
     return HttpResponse("<pre>" + "\n".join(debug_info) + "</pre>")
